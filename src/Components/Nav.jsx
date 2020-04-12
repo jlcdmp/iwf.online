a3a15873e0b7fda3e1a47efdcb3a5b8d603b7922
jlcdmp <joeoliverc123@gmail.com>
2020-04-12 16:07:41 +0100

    nav postion bug fixed

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -16,50 +16,51 @@ class Nav extends Component {
   render() {
     return (
 
-      <>
-        <ClickOutside
-          onClickOutside={() => {
-            this.setState({ expanded: false });
+
+      <ClickOutside
+        onClickOutside={() => {
+          this.setState({ expanded: false });
+        }}
+      >
+
+        <SideNav
+          expanded={this.state.expanded}
+          onToggle={(expanded) => {
+            this.setState({ expanded });
           }}
         >
 
-          <SideNav
-            expanded={this.state.expanded}
-            onToggle={(expanded) => {
-              this.setState({ expanded });
-            }}
-          >
+          <SideNav.Toggle />
+          <SideNav.Nav defaultSelected="home">
+            <NavItem eventKey="home">
+              <NavIcon>
+                <i className="fa fa-fw fa-home" style={{ fontSize: '3em' }} />
+              </NavIcon>
+              <NavText>
+                Home
+            </NavText>
+            </NavItem>
+            <NavItem eventKey='services'>
+              <NavIcon>
+                <i style={{ fontSize: '3em' }} />
+              </NavIcon>
+              <NavText>
+                Our Services
+            </NavText>
+            </NavItem>
+            <NavItem eventKey='contact'>
+              <NavIcon>
+                <i style={{ fontSize: '3em' }} />
+              </NavIcon>
+              <NavText>
+                Contact Us
+            </NavText>
+            </NavItem>
+          </SideNav.Nav>
+        </SideNav>
 
-            <SideNav.Toggle />
-            <SideNav.Nav defaultSelected="home">
-              <NavItem eventKey="home">
-                <NavIcon>
-                  <i className="fa fa-fw fa-home" style={{ fontSize: '3em' }} />
-                </NavIcon>
-                <NavText>
-                  Home
-            </NavText>
-              </NavItem>
-              <NavItem eventKey='services'>
-                <NavIcon>
-                  <i style={{ fontSize: '3em' }} />
-                </NavIcon>
-                <NavText>
-                  Our Services
-            </NavText>
-              </NavItem>
-              <NavItem eventKey='contact'>
-                <NavIcon>
-                  <i style={{ fontSize: '3em' }} />
-                </NavIcon>
-                <NavText>
-                  Contact Us
-            </NavText>
-              </NavItem>
-            </SideNav.Nav>
-          </SideNav>
-        </ClickOutside>
-      </>
+      </ClickOutside>
+
     );
   }
 }
